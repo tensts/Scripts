a93e311494c2afc1c68f657fbc55df694225d96c
krystian <krystian@WAWKOR01>
2017-11-10 14:22:34 +0100

    fixed bug with mount point containing trailing slash

--- cmount/cumount.sh
+++ cmount/cumount.sh
@@ -7,6 +7,10 @@ echo "nie podales punktu montowania"
 exit 1
 fi
 
+if [ "${MOUNT_POINT: -1}" == "/" ]; then
+    MOUNT_POINT="${MOUNT_POINT::-1}"
+fi
+
 if [ $EUID -ne 0 ]; then
 echo "musisz uruchomic jako root"
 exit 1
@@ -15,14 +19,15 @@ fi
 echo "syncing..."
 sync
 echo "done"
+
+CUUID=$(mount | grep $MOUNT_POINT | cut -d " " -f1 | cut -d "/" -f4)
+
 umount $MOUNT_POINT
 if [ ! $? -eq 0 ]; then
 echo "nie udalo sie odmontowac"
-#exit 1
+exit 1
 fi
 
-
-CUUID=$(mount | grep $MOUNT_POINT | cut -d " " -f1 | cut -d "/" -f4)
 echo $CUUID
 echo "close luks"
 cryptsetup close $CUUID
